085db54529f80efa287ea51cb8d6f8a0f1b5492f
ksh9241 <ksh9241@naver.com>
2021-03-11 16:28:00 +0900

    Add my_module [Mul]

--- my_module.java
+++ my_module.java
@@ -7,9 +7,13 @@ public class my_module{
 	   System.out.println(a-b);
 	}
 
+	public void mul(int a, int b){
+	   System.out.println(a*b);
+	}
 	public static void main(String[] args){
 	     my_module m = new my_module();
 	     m.add(10,5);
 	     m.sub(17,3);
+	     m.mul(3,5);
 	}
 }
