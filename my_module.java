e29ce7246a0a77111bce5ffbda8fba2a58b61d50
ksh9241 <ksh9241@naver.com>
2021-03-11 16:07:56 +0900

    Add my_module [Add & Sub]

---
+++ my_module.java
@@ -0,0 +1,15 @@
+public class my_module{
+	public void add(int a,int b){
+	   System.out.println(a+b);
+	}
+
+	public void sub(int a,int b){
+	   System.out.println(a-b);
+	}
+
+	public static void main(String[] args){
+	     my_module m = new my_module();
+	     m.add(10,5);
+	     m.sub(17,3);
+	}
+}
