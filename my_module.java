2cccefb669b8c6a0b4be519cfac171113db6f277
ksh9241 <ksh9241@naver.com>
2021-03-11 17:09:07 +0900

    Add my_module [Div] & Comment

--- my_module.java
+++ my_module.java
@@ -10,10 +10,17 @@ public class my_module{
 	public void mul(int a, int b){
 	   System.out.println(a*b);
 	}
+
+	public void div(int a, int b){
+	   System.out.println(a/b);
+	}
 	public static void main(String[] args){
 	     my_module m = new my_module();
 	     m.add(10,5);
 	     m.sub(17,3);
 	     m.mul(3,5);
+	     m.div(8,2);
+	     //주석
 	}
+
 }
